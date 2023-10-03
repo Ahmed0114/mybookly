992fa4fa43e8e80756cbb905c402dcafd0179434
ahmed <ahmedthurwat@gmail.com>
2023-10-03 12:43:07 +0300

    addstylesfile&texttohome

---
+++ lib/core/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+abstract class styles{
+   static const midstyle =TextStyle(
+     fontWeight: FontWeight.w600,
+     fontSize: 18,
+
+   );
+
+}
--- lib/feature/home/data/presentation/viewmodels/views/widgets/home_body.dart
+++ lib/feature/home/data/presentation/viewmodels/views/widgets/home_body.dart
@@ -1,17 +1,25 @@
 
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:my_bookly/core/styles.dart';
 import 'custom_app_bar.dart';
 import 'custom_list_view_item.dart';
 class home_body extends StatelessWidget {
   const home_body({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-         customappbar(),
-        feature_list_view(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+           customappbar(),
+          feature_list_view(),
+          SizedBox(
+            height: 20,
+          ),
+          Text('Best Seller',style: styles.midstyle)
+        ],
+      ),
     );
   }
 }
@@ -26,7 +34,7 @@ class feature_list_view extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemBuilder: (context,index){
           return const Padding(
-            padding:  EdgeInsets.symmetric(horizontal: 8),
+            padding:  EdgeInsets.symmetric(horizontal: 0),
             child:  custom_list_view_item(),
           );
         },
